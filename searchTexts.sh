1fa45a4b48b6cf7e4cf071362e4fac7f37db3b1e
Shane S <gs@shanewilliamscott.com>
2022-02-24 16:08:03 -0600

    Updated

--- searchTexts.sh
+++ searchTexts.sh
@@ -1,17 +1,101 @@
 #!/bin/bash
 
-searchTermsCase=("AKIA", "ASIA", "MII", "eyJ",)
-searchTermsAnyCase=("AccessKey", "AccessSecret", "Secret", "Token", "Decrypted", "Encrypted", "Password", "Username", "SecretKey", "Key" "\.decode", "Bearer", "X-API", "https:\/\/", "http:\/\/",)
+if [ "$#" -lt 1 ] || [ "$#" -gt 1 ]
+then
+    echo  "Usage: ./searchTexts.sh /my/path/"
+    exit 1
+else
+    echo -e "\c"
+fi
+
+if [ ! -d "$1" ]; then
+    echo "Get real. $1 doesn't exist."
+    exit 1
+fi
+
+searchTermsCase=(
+    'AKIA'
+    'ASIA'
+    'MII'
+    'eyJ'
+    'SHA\/1'
+    'SHA\/256'
+    'sq0csp-'
+    'Authorization'
+    'BEGIN RSA'
+    'BEGIN DSA'
+    'BEGIN EC'
+    'BEGIN PRIVATE'
+    'BEGIN OPENSSH'
+    'BEGIN SSH'
+    'BEGIN PGP'
+    'PuTTY-User-Key'
+)
+
+searchTermsAnyCase=(
+    'AccessKey'
+    'AccessSecret'
+    'Secret'
+    'Token'
+    'Decrypted'
+    'Encrypted'
+    'Pass'
+    'Pwd'
+    'Username'
+    'SecretKey'
+    'Key'
+    '\.decode'
+    '\.encode'
+    'Bearer'
+    'X-API'
+    'https:'
+    'http:'
+    'webhooks'
+    'AccountKey'
+    'SubscriptionId'
+    'RegionId'
+    'contrase'
+    'Realm'
+    'Domain'
+    'SAML'
+)
+
+searchTermsRegEx=(
+    'api_?.*key'
+    'auth_?.*key'
+    'service_?.*key'
+    'account_?.*key'
+    'db_?.*key'
+    'database_?.*key'
+    'priv_?.*key'
+    'private_?.*key'
+    'client_?.*key'
+    'db_?.*pass'
+    'database_?.*pass'
+    'key_?.*pass'
+    'AC[a-z0-9]{32}'
+    'SK[a-z0-9]{32}'
+)
+
+
+for term in ${searchTermsRegEx[@]}
+    do
+        echo ""
+        echo "Searching (regex term) ${term}..."
+        grep -E ${term} $1* -R
+    done
 
 for term in ${searchTermsCase[@]}
     do
+        echo ""
         echo "Searching (cased) ${term}..."
-        grep -e "${term}" $1/* -R
+        grep -e ${term} $1* -R
     done
 
 
 for term in ${searchTermsAnyCase[@]}
     do
+        echo ""
         echo "Searching (case insensitive) ${term}..."
-        grep -e "${term}" $1/* -Ri
+        grep -e ${term} $1* -Ri
     done
---
+++ testMe.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./searchTexts.sh ./fixtures
